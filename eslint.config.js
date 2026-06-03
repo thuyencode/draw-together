d4385a3a7d652fb077639ce9af1b910cf0bad314
Thuyen Code <thuyencode@proton.me>
2026-06-03 08:36:00 +0700

    Add Storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,32 @@
 // @ts-check
 
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import * as tsParser from "@typescript-eslint/parser";
-import { defineConfig } from "eslint/config";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 import solid from "eslint-plugin-solid/configs/typescript";
+import { defineConfig } from "eslint/config";
 import ts from "typescript-eslint";
 
 export default [
-	...defineConfig(
-		js.configs.recommended,
-		ts.configs.recommended,
-		ts.configs.strict,
-		ts.configs.stylistic,
-	),
-	{
-		files: ["**/*.{ts,tsx}"],
-		...solid,
-		languageOptions: {
-			parser: tsParser,
-			parserOptions: {
-				project: "tsconfig.json",
-			},
-		},
-	},
+  ...defineConfig(
+    js.configs.recommended,
+    ts.configs.recommended,
+    ts.configs.strict,
+    ts.configs.stylistic,
+  ),
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...solid,
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+  },
+  ...storybook.configs["flat/recommended"],
+  jsxA11y.flatConfigs.recommended,
 ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,46 @@
+/// <reference types="vitest/config" />
 import { solidStart } from "@solidjs/start/config";
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { playwright } from "@vitest/browser-playwright";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-	plugins: [solidStart(), tailwindcss(), nitro()],
+  plugins: [solidStart(), tailwindcss(), nitro()],
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+        },
+      },
+    ],
+  },
 });
