d53449732f173170d4cbcb3b3a5ce3a584c82ac6
Thuyen Code <thuyencode@proton.me>
2026-06-03 09:49:18 +0700

    Fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,29 @@
 // @ts-check
 
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from "@eslint/js";
 import * as tsParser from "@typescript-eslint/parser";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import solid from "eslint-plugin-solid/configs/typescript";
-import { defineConfig } from "eslint/config";
+import storybook from "eslint-plugin-storybook";
+import { defineConfig, globalIgnores } from "eslint/config";
 import ts from "typescript-eslint";
 
 export default [
-  ...defineConfig(
-    js.configs.recommended,
-    ts.configs.recommended,
-    ts.configs.strict,
-    ts.configs.stylistic,
-  ),
+  ...defineConfig([
+    {
+      files: ["src/**/*.{ts,tsx}"],
+      extends: [
+        js.configs.recommended,
+        ts.configs.strict,
+        ts.configs.stylistic,
+      ],
+    },
+    globalIgnores(["storybook-static/**/*", ".storybook/**/*"]),
+  ]),
+  ...storybook.configs["flat/recommended"],
+  jsxA11y.flatConfigs.recommended,
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["src/**/*.{ts,tsx}"],
     ...solid,
     languageOptions: {
       parser: tsParser,
@@ -27,6 +32,4 @@ export default [
       },
     },
   },
-  ...storybook.configs["flat/recommended"],
-  jsxA11y.flatConfigs.recommended,
 ];
