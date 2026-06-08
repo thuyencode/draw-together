a47ed8446f8595991940f82626f581644c1a726f
Thuyen Code <thuyencode@proton.me>
2026-06-08 17:21:29 +0700

    Temporarily disable unused addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,8 @@ import type { StorybookConfig } from "storybook-solidjs-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
+    // "@chromatic-com/storybook",
+    // "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
   ],
