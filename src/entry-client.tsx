50b65d366e7f551d76424302ba3be5a042a91c9a
Thuyen Code <thuyencode@proton.me>
2026-06-08 15:40:56 +0700

    Add null check for `app` element and set body background class

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,10 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
-mount(() => <StartClient />, document.getElementById("app")!);
+const app = document.getElementById("app");
+
+if (!app) {
+  throw new Error("can't find element with the id 'app'");
+}
+
+mount(() => <StartClient />, app);
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -11,7 +11,7 @@ export default createHandler(() => (
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
-        <body>
+        <body class="bg-background-soft-100">
           <div id="app">{children}</div>
           {scripts}
         </body>
