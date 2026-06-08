758d2034c8767eeaafe17363901161e6774007ee
Thuyen Code <thuyencode@proton.me>
2026-06-08 15:41:31 +0700

    Prettier them all

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,13 @@
-import type { StorybookConfig } from 'storybook-solidjs-vite';
+import type { StorybookConfig } from "storybook-solidjs-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
   ],
-  "framework": "storybook-solidjs-vite"
+  framework: "storybook-solidjs-vite",
 };
 export default config;
