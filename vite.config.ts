efd89ad6b2269b0b1e19b07407c9cceb30ea2304
Thuyen Code <thuyencode@proton.me>
2026-06-11 14:06:41 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-/// <reference types="vitest/config" />
 import { solidStart } from "@solidjs/start/config";
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import { playwright } from "@vitest/browser-playwright";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
@@ -16,33 +13,33 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [solidStart(), tailwindcss(), nitro()],
-  test: {
-    projects: [
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, ".storybook"),
-          }),
-        ],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [
-              {
-                browser: "chromium",
-              },
-            ],
-          },
-        },
-      },
-    ],
-  },
+  // test: {
+  //   projects: [
+  //     {
+  //       extends: true,
+  //       plugins: [
+  //         // The plugin will run tests for the stories defined in your Storybook config
+  //         // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+  //         storybookTest({
+  //           configDir: path.join(dirname, ".storybook"),
+  //         }),
+  //       ],
+  //       test: {
+  //         name: "storybook",
+  //         browser: {
+  //           enabled: true,
+  //           headless: true,
+  //           provider: playwright({}),
+  //           instances: [
+  //             {
+  //               browser: "chromium",
+  //             },
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
   resolve: {
     alias: {
       "~": path.resolve(dirname, "./src"),
