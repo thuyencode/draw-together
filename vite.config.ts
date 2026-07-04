612ff54fef467a930463fbc67ebe8aa8c24dc01a
Thuyen Code <thuyencode@proton.me>
2026-07-04 09:25:26 +0700

    Enable `tsconfigPaths` and use regex in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,25 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
 import { analyzer, unstableRolldownAdapter } from "vite-bundle-analyzer";
-
-const dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [
     unstableRolldownAdapter(analyzer({ analyzerMode: "server" })),
     devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    solid({ ssr: true }),
     nitro({
       preset: "bun",
       compressPublicAssets: true,
     }),
-    tailwindcss(),
-    tanstackStart(),
-    solid({ ssr: true }),
   ],
   resolve: {
-    alias: {
-      "~": path.resolve(dirname, "./src"),
-      "fabricjs-psbrush": path.resolve(dirname, "./fabricjs-psbrush/lib"),
-    },
+    tsconfigPaths: true,
   },
   build: {
     rolldownOptions: {
@@ -37,17 +27,14 @@ export default defineConfig({
         codeSplitting: {
           groups: [
             {
-              name(id) {
-                if (id.includes("node_modules/@ark-ui")) {
-                  return "ark-ui";
-                }
-
-                if (id.includes("node_modules/fabric")) {
-                  return "fabric";
-                }
-
-                return null;
-              },
+              name: "ark-ui",
+              test: /node_modules[\\/]@ark-ui/,
+              priority: 20,
+            },
+            {
+              name: "fabric",
+              test: /node_modules[\\/]fabric/,
+              priority: 15,
             },
           ],
         },
