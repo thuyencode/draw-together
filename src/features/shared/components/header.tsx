7ec02c6324d28dc8c88d672ead83e00946507dc7
Thuyen Code <thuyencode@proton.me>
2026-07-28 21:24:11 +0700

    Adjust header ui

--- src/features/shared/components/header.tsx
+++ src/features/shared/components/header.tsx
@@ -6,8 +6,8 @@ export function Header() {
   const location = useLocation();
 
   return (
-    <Show when={!location().pathname.startsWith("/rooms")}>
-      <header class="border-neutral/20 bg-base-100 fixed inset-x-0 top-0 z-100 mx-auto w-full max-w-3xl border px-4 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-md lg:max-w-5xl">
+    <Show when={!location().pathname.startsWith("/rooms/trial")}>
+      <header class="border-neutral/20 bg-base-100 dark:bg-base-300 fixed inset-x-0 top-0 z-100 mx-auto w-full max-w-3xl border px-4 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-md lg:max-w-5xl">
         <NavBar />
       </header>
     </Show>
